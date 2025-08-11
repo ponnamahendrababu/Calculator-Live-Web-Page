39897c7df5c2ccda5a464e66994dd6a483ca1fd3
mahendra babu ponna <ponnamahendra211@gmail.com>
2025-08-11 17:29:24 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,39 @@
+const inputValue = document.getElementById('user-input');
+const number = document.querySelectorAll('.numbers');
+const calculate = document.querySelectorAll('.operations');
+
+number.forEach((item) =>{
+    item.addEventListener('click', (e)=> {
+        if(inputValue.innerText === 'NaN'){
+            inputValue.innerText='';
+        }
+        if(inputValue.innerText === '0'){
+            inputValue.innerText = '';
+        }
+        inputValue.innerText += e.target.innerHTML.trim();
+    })
+
+})
+
+
+calculate.forEach((item)=>{
+    item.addEventListener('click',(e)=>{
+        let lastValue = inputValue.innerText.substring(inputValue.innerText.length, inputValue.innerText.length - 1);
+
+        if(!isNaN(lastValue) && e.target.innerHTML === '='){
+            inputValue.innerText = eval(inputValue.innerText);
+        }else if(e.target.innerHTML === 'AC'){
+            inputValue.innerText = 0;
+        }else if(e.target.innerHTML === 'DEL'){
+            inputValue.innerText = inputValue.innerText.substring(0,inputValue.innerText.length-1);
+
+            if(inputValue.innerText.length == 0){
+                inputValue.innerText = 0;
+            }
+        }else{
+            if(!isNaN(lastValue)){
+                inputValue.innerText += e.target.innerHTML;
+            }
+        }
+    })
+})
